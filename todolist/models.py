5fcc9359dabc104a0cc54a9350ac63cad428e52e
Hanka <hrosicka@post.cz>
2025-05-01 10:38:46 +0200

    task category added

--- todolist/models.py
+++ todolist/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     description = models.CharField(max_length=200)
     resolved = models.BooleanField(default=False)
@@ -17,6 +23,7 @@ class Task(models.Model):
     )
 
     date_created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.popis
--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import AddTaskForm, FilterPriorityForm
+from .forms import AddTaskForm, FilterPriorityForm, FilterCategoryForm
 from .models import Task
 from django.db.models import Case, When, Value, IntegerField
 
@@ -15,7 +15,8 @@ def add_task(request):
     return render(request, 'todolist/add_task.html', {'form': form})
 
 def task_list(request):
-    filter_form = FilterPriorityForm(request.GET)
+    priority_filter_form = FilterPriorityForm(request.GET)
+    category_filter_form = FilterCategoryForm(request.GET)
     tasks = Task.objects.annotate(
         priority_order=Case(
             When(priority='height', then=Value(1)),
@@ -31,8 +32,8 @@ def task_list(request):
     else:
         tasks = tasks.order_by('priority_order')
 
-    if filter_form.is_valid():
-        priority_filter = filter_form.cleaned_data.get('priority')
+    if priority_filter_form.is_valid():
+        priority_filter = priority_filter_form.cleaned_data.get('priority')
         if priority_filter:
             tasks = tasks.filter(priority=priority_filter)
             if order:
@@ -40,7 +41,21 @@ def task_list(request):
             else:
                 tasks = tasks.order_by('priority_order')
 
-    return render(request, 'todolist/task_list.html', {'tasks': tasks, 'filter_form': filter_form})
+    if category_filter_form.is_valid():
+        category_filter = category_filter_form.cleaned_data.get('category')
+        if category_filter:
+            tasks = tasks.filter(category=category_filter)
+            if order:
+                tasks = tasks.order_by(order)
+            else:
+                tasks = tasks.order_by('priority_order')
+
+    context = {
+        'tasks': tasks,
+        'priority_filter_form': priority_filter_form,
+        'category_filter_form': category_filter_form,
+    }
+    return render(request, 'todolist/task_list.html', context)
 
 def tag_resolved(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
@@ -59,7 +74,7 @@ def edit_task(request, task_id):
         form = AddTaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-            return redirect('seznam_ukolu')
+            return redirect('task_list')
     else:
         form = AddTaskForm(instance=task)
     return render(request, 'todolist/edit_task.html', {'form': form, 'task': task})
